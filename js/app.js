606986db83f75bd5c30205a773fd2538fee4f998
rakibul-cse-diu <rakibul2180@gmail.com>
2022-03-11 12:31:48 +0600

    question answer section added

--- js/app.js
+++ js/app.js
@@ -33,18 +33,21 @@ const displayContent = (text) => {
 const switchTab = (id) => {
   if (id === "posts") {
     document.getElementById("posts").style.display = "grid";
+    document.getElementById("question-answer").style.display = "block";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("question-answer").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("question-answer").style.display = "none";
 
     displayReportedPosts();
   }
@@ -52,7 +55,6 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
-  console.log(post)
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
